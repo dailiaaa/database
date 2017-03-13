ba3b3ca521ac54a05aa21ed0a161df83dc00f6ae
once <none>
2017-03-13 16:01:55 +0800

    database related file

---
+++ forumdb.py
@@ -0,0 +1,42 @@
+#
+# Database access functions for the web forum.
+#
+
+import time
+import psycopg2
+
+## Database connection
+
+
+## Get posts from database.
+def GetAllPosts():
+    '''Get all the posts from the database, sorted with the newest first.
+
+    Returns:
+      A list of dictionaries, where each dictionary has a 'content' key
+      pointing to the post content, and 'time' key pointing to the time
+      it was posted.
+    '''
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute('SELECT time,content FROM posts ORDER BY time DESC')
+    rows = c.fetchall()
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in rows]
+    DB.close()
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    return posts
+
+## Add a post to the database.
+def AddPost(content):
+    '''Add a new post to the database.
+
+    Args:
+      content: The text content of the new post.
+    '''
+    t = time.strftime('%c', time.localtime())
+    #DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("INSERT INTO posts VALUES (%s)", (content,))
+    DB.commit()
+    DB.close()
